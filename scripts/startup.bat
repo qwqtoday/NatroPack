6e4f25010e096b9ad8e318454ad90f03f76692b0
qwqtoday <lukas.leung@hotmail.com>
2024-11-14 16:40:58 +0800

    Start RDP in startup script

--- scripts/startup.bat
+++ scripts/startup.bat
@@ -2,12 +2,14 @@
 
 setlocal enabledelayedexpansion
 set b=0
+set i=2
 for /f %%a in ('net localgroup natro') do (
     IF "!b!"=="1" (
         IF "%%a"=="The" (
             goto break
         )
-        echo %%a
+        C:\NatroPack\bin\rdp.exe /v:127.0.0.!i! /u:%%a /p:123456 /w:1280 /h:720
+        set /a i=i+1
     )
     if "%%a"=="-------------------------------------------------------------------------------" (
         set b=1
