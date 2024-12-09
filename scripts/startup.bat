d645678586d111432a305e48115b716d97d9318f
qwqtoday <lukas.leung@hotmail.com>
2024-12-09 14:55:10 +0800

    optimization for rdp startup

--- scripts/startup.bat
+++ scripts/startup.bat
@@ -10,7 +10,7 @@ for /f %%a in ('net localgroup natro') do (
         IF "%%a"=="The" (
             goto break
         )
-        C:\NatroPack\bin\rdp.exe /v:127.0.0.!i! /u:%%a /p:%password% /w:1366 /h:768
+        C:\NatroPack\bin\rdp.exe /v:127.0.0.!i! /u:%%a /p:%password% /nodrives /nosound /nowallpaper /w:1366 /h:768
         set /a i=i+1
     )
     if "%%a"=="-------------------------------------------------------------------------------" (
