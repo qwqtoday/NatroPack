d465b3a3a1213f9eab8eba4b1059d329a6a43b6b
qwqtoday <lukas.leung@hotmail.com>
2024-12-15 16:01:40 +0800

    remove local

--- scripts/connect-user.bat
+++ scripts/connect-user.bat
@@ -1,4 +1,3 @@
-setlocal EnableDelayedExpansion
 @echo off
 
 set ip=%1
@@ -10,4 +9,3 @@ set /p title=<C:\NatroPack\data\users\%username%\title.txt
 if not "%title%" == "" set titlearg=/title:"%title%"
 
 C:\NatroPack\bin\rdp.exe /v:%ip% /u:%username% /p:%password% /nodrives /nosound /nowallpaper /w:1366 /h:768 %titlearg%
-endlocal
