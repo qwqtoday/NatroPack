830ebf2df4a5281e67870121fd24dcb0ddfafe2f
qwqtoday <lukas.leung@hotmail.com>
2024-12-15 16:18:40 +0800

    a

--- scripts/startup.bat
+++ scripts/startup.bat
@@ -6,12 +6,12 @@ setlocal EnableDelayedExpansion
 set b=0
 set i=2
 for /f %%a in ('net localgroup natro') do (
-    IF "!b!"=="1" (
+    IF "%b%"=="1" (
         IF "%%a"=="The" (
             goto break
         )
 
-        "C:\NatroPack\scripts\connect-user.bat" "127.0.0.!i!" "%%a" "%password%"
+        "C:\NatroPack\scripts\connect-user.bat" "127.0.0.%i%" "%%a" "%password%"
         set /a i=i+1
     )
     if "%%a"=="-------------------------------------------------------------------------------" (
