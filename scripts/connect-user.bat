1070498e35659b451a504048e25d91ee0af6ed04
qwqtoday <lukas.leung@hotmail.com>
2024-12-15 15:27:47 +0800

    fix title issue

--- scripts/connect-user.bat
+++ scripts/connect-user.bat
@@ -7,6 +7,6 @@ set password=%3
 
 set /p title=<C:\NatroPack\data\users\%username%\title.txt
 
-if not [%title%] == [] set titlearg=/title:"%title%"
+if not "%title%" == "" set titlearg=/title:"%title%"
 
 C:\NatroPack\bin\rdp.exe /v:%ip% /u:%username% /p:%password% /nodrives /nosound /nowallpaper /w:1366 /h:768 %titlearg%
