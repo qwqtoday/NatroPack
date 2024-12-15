ad2b212ea775b4fe6caf6d3df60be208da03c5ea
qwqtoday <lukas.leung@hotmail.com>
2024-12-15 15:30:23 +0800

    not sure

--- scripts/connect-user.bat
+++ scripts/connect-user.bat
@@ -10,3 +10,4 @@ set /p title=<C:\NatroPack\data\users\%username%\title.txt
 if not "%title%" == "" set titlearg=/title:"%title%"
 
 C:\NatroPack\bin\rdp.exe /v:%ip% /u:%username% /p:%password% /nodrives /nosound /nowallpaper /w:1366 /h:768 %titlearg%
+endlocal
